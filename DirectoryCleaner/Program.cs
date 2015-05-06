9d8bde4e29eeb4a92e455d9df3e6da9d96dcf085
Miles Edwards <miles@mylar.me>
2015-05-06 11:47:21 +0100

    Initial Commit

---
+++ DirectoryCleaner/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Removed Directories: " + EmptyDirectory(new DirectoryInfo(args[0])));
+                Console.ReadLine();
+            }            
+        }
+
+        static int EmptyDirectory(DirectoryInfo Directory)
+        {
+            int removedDirectories = 0;
+
+            foreach (DirectoryInfo NestedDirectory in Directory.GetDirectories())
+            {
+                removedDirectories += EmptyDirectory(NestedDirectory);
+            }
+
+            try
+            {
+                if (Directory.GetFiles().Length == 0 && Directory.GetDirectories().Length == 0)
+                {
+                    Console.Write("Removing " + Directory.FullName + "...");
+
+                    try
+                    {
+                        Directory.Delete();
+                        removedDirectories++;
+
+                        Console.WriteLine(" Success");
+                    }
+                    catch
+                    {
+                        Console.WriteLine(" Failed");
+                    }
+                }
+            }
+            catch { }
+
+            return removedDirectories;        
+        }
+    }
+}
---
+++ DirectoryCleaner/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EmptyDirectory")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmptyDirectory")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d45b252d-246f-4302-a46d-ef051d83130c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
